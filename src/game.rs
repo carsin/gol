6d76c91e3f0a9d5763680324189e86063ea467c4
carsin <carsin@users.noreply.github.com>
2020-07-09 11:55:53 -1000

    destruct camera from game

--- src/game.rs
+++ src/game.rs
@@ -10,40 +10,59 @@ enum Direction {
     West,
 }
 
+pub struct Camera {
+    viewport_width: usize,
+    viewport_height: usize,
+    x: usize,
+    y: usize,
+    speed: usize,
+}
+
+impl Camera {
+    fn new(x: usize, y: usize) -> Self {
+        let viewport_dimensions = terminal::size().unwrap();
+        // Width is half of terminal because each cell is rendered as 2 characters wide
+        let viewport_width = (viewport_dimensions.0 / 2) as usize;
+        let viewport_height = viewport_dimensions.1 as usize;
+
+        Self {
+            viewport_width,
+            viewport_height,
+            x,
+            y,
+            speed: 5,
+        }
+    }
+
+    fn pan(&mut self, dir: Direction, max_width: usize, max_height: usize) {
+        match dir {
+            Direction::North => self.y = util::clamp(self.y.checked_sub(self.speed).unwrap_or(0), 0, max_height),
+            Direction::South => self.y = util::clamp(self.y + self.speed, 0, max_height),
+            Direction::East => self.x = util::clamp(self.x + self.speed, 0, max_width),
+            Direction::West => self.x = util::clamp(self.x.checked_sub(self.speed).unwrap_or(0), 0, max_width)
+        }
+    }
+}
+
 pub struct Game {
     pub stdout: std::io::Stdout,
     pub map: map::Map,
     pub running: bool,
     pub paused: bool,
-    pub viewport_width: usize,
-    pub viewport_height: usize,
-    pub camera_x: usize,
-    pub camera_y: usize,
-    pub camera_speed: usize,
+    pub camera: Camera,
     pub update_count: usize,
     pub updates_per_second: f32,
 }
 
 impl Game {
     pub fn new(stdout: std::io::Stdout, map: map::Map) -> Self {
-        let viewport_dimensions = terminal::size().unwrap();
-        // Width is half of terminal because each cell is rendered as 2 characters wide
-        let viewport_width = (viewport_dimensions.0 / 2) as usize;
-        let viewport_height = viewport_dimensions.1 as usize;
-
-        let camera_x = map.width / 2;
-        let camera_y = map.height / 2;
-
-        Game {
+        let temp = [map.width, map.height];
+        Self {
             stdout,
             map,
             running: false,
             paused: false,
-            viewport_width,
-            viewport_height,
-            camera_x,
-            camera_y,
-            camera_speed: 4,
+            camera: Camera::new(temp[0] / 2, temp[1] / 2),
             update_count: 0,
             updates_per_second: 5.0,
         }
@@ -55,11 +74,11 @@ impl Game {
     }
 
     pub fn render_map(&mut self) {
-        for x in 0..self.viewport_width {
-            for y in 1..self.viewport_height {
+        for x in 0..self.camera.viewport_width {
+            for y in 1..self.camera.viewport_height {
                 // Calculate the position to render, None if negative (can't access negative array indicies)
-                let x_pos = (x + self.camera_x).checked_sub((self.viewport_width / 2) as usize);
-                let y_pos = (y + self.camera_y).checked_sub((self.viewport_height / 2) as usize);
+                let x_pos = (x + self.camera.x).checked_sub((self.camera.viewport_width / 2) as usize);
+                let y_pos = (y + self.camera.y).checked_sub((self.camera.viewport_height / 2) as usize);
                 let positions = [x_pos, y_pos];
 
                 let chars_to_print = match positions {
@@ -68,7 +87,7 @@ impl Game {
                         // Check if position is valid (within array)
                         if let Some(pos) = self.map.pos(x, y) {
                             match self.map.cells[pos] {
-                                false => ". ",
+                                false => "  ",
                                 true => "██",
                             }
                         } else {
@@ -95,12 +114,12 @@ impl Game {
             .unwrap()
             .queue(terminal::Clear(terminal::ClearType::CurrentLine))
             .unwrap()
-            .queue(Print(format!("X: {}, Y: {} ─── Camera Speed: {}", self.camera_x, self.camera_y, self.camera_speed)))
+            .queue(Print(format!("X: {}, Y: {} ─── Camera Speed: {}", self.camera.x, self.camera.y, self.camera.speed)))
             .unwrap();
         if self.paused {
             let paused_text = "─ PAUSED ─";
             self.stdout
-                .queue(cursor::MoveTo((self.viewport_width - paused_text.len()) as u16, 0))
+                .queue(cursor::MoveTo((self.camera.viewport_width - paused_text.len()) as u16, 0))
                 .unwrap()
                 .queue(Print(format!("{}", paused_text)))
                 .unwrap();
@@ -108,7 +127,7 @@ impl Game {
 
         let right_status_text = format!("Alive Cells: {} ─── Gen: {}", self.map.live_cell_count, self.update_count);
         self.stdout
-            .queue(cursor::MoveTo(((self.viewport_width * 2) - right_status_text.len()) as u16, 0))
+            .queue(cursor::MoveTo(((self.camera.viewport_width * 2) - right_status_text.len()) as u16, 0))
             .unwrap()
             .queue(Print(format!("{}", right_status_text)))
             .unwrap();
@@ -117,8 +136,8 @@ impl Game {
     pub fn process_mouse_input(&mut self, input: event::MouseEvent) {
         match input {
             event::MouseEvent::Down(button, click_x, click_y, _) | event::MouseEvent::Drag(button, click_x, click_y, _) => {
-                let click_x_index = (self.camera_x + (click_x / 2) as usize).checked_sub(self.viewport_width / 2);
-                let click_y_index = (self.camera_y + click_y as usize).checked_sub(self.viewport_height / 2);
+                let click_x_index = (self.camera.x + (click_x / 2) as usize).checked_sub(self.camera.viewport_width / 2);
+                let click_y_index = (self.camera.y + click_y as usize).checked_sub(self.camera.viewport_height / 2);
 
                 let positions = [click_x_index, click_y_index];
                 match positions {
@@ -146,37 +165,17 @@ impl Game {
     pub fn process_key_input(&mut self, input: event::KeyCode) {
         match input {
             event::KeyCode::Char('q') => self.running = false,
-            event::KeyCode::Char('w') | event::KeyCode::Char('k') => self.move_camera(Direction::North),
-            event::KeyCode::Char('a') | event::KeyCode::Char('h') => self.move_camera(Direction::West),
-            event::KeyCode::Char('s') | event::KeyCode::Char('j') => self.move_camera(Direction::South),
-            event::KeyCode::Char('d') | event::KeyCode::Char('l') => self.move_camera(Direction::East),
+            event::KeyCode::Char('w') | event::KeyCode::Char('k') => self.camera.pan(Direction::North, self.map.width, self.map.height),
+            event::KeyCode::Char('a') | event::KeyCode::Char('h') => self.camera.pan(Direction::West, self.map.width, self.map.height),
+            event::KeyCode::Char('s') | event::KeyCode::Char('j') => self.camera.pan(Direction::South, self.map.width, self.map.height),
+            event::KeyCode::Char('d') | event::KeyCode::Char('l') => self.camera.pan(Direction::East, self.map.width, self.map.height),
             event::KeyCode::Char('c') => { self.map.clear_map(); self.update_count = 0; },
-            event::KeyCode::Char('+') => self.camera_speed = util::clamp(self.camera_speed + 1, 1, 10),
-            event::KeyCode::Char('-') => self.camera_speed = util::clamp(self.camera_speed - 1, 1, 10),
+            event::KeyCode::Char('+') => self.camera.speed = util::clamp(self.camera.speed + 1, 1, 10),
+            event::KeyCode::Char('-') => self.camera.speed = util::clamp(self.camera.speed - 1, 1, 10),
             event::KeyCode::Char('r') => self.map.randomize_map(),
             event::KeyCode::Char(' ') => self.paused = !self.paused,
             _ => (),
         }
-    }
-
-    fn move_camera(&mut self, dir: Direction) {
-        match dir {
-            Direction::North => {
-                self.camera_y = util::clamp(self.camera_y.checked_sub(self.camera_speed).unwrap_or(0), 0, self.map.height);
-            }
-            Direction::South => {
-                self.camera_y = util::clamp(self.camera_y + self.camera_speed, 0, self.map.height);
-            }
-            Direction::East => {
-                self.camera_x = util::clamp(self.camera_x + self.camera_speed, 0, self.map.width);
-            }
-            Direction::West => {
-                self.camera_x = util::clamp(self.camera_x.checked_sub(self.camera_speed).unwrap_or(0), 0, self.map.width);
-            }
-        }
-
-        self.render_status();
-        self.render_map();
     }
 
     pub fn resize_viewport(&mut self, width: usize, height: usize) {
@@ -184,7 +183,7 @@ impl Game {
             .execute(terminal::Clear(terminal::ClearType::All))
             .unwrap();
 
-        self.viewport_width = width / 2;
-        self.viewport_height = height;
+        self.camera.viewport_width = width / 2;
+        self.camera.viewport_height = height;
     }
 }
